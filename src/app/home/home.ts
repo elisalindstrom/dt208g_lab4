1bef7538c526a676210920eb7e0b2b7ffaee8afa
elisalindstrom <elisalindstroem@gmail.com>
2026-05-05 17:51:10 +0200

    lagt till async och förbättrat funktioner som hämtar och laddar data

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Component, inject, signal } from '@angular/core';
 import { CourseService } from '../services/course-service';
+import { Course } from '../interfaces/course'; // Import av interface
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  courses: any;
+  courses = signal<Course[]>([]);
+  error = signal<string | null>(null);
+  courseService = inject(CourseService);
 
-  constructor(private courseService: CourseService) {
+  /* constructor(private courseService: CourseService) {
     this.courses = toSignal(this.courseService.getCourses(), {
       initialValue: []
     });
+  } */
+
+  ngOnInit() {
+    this.loadCourses();
+  }
+
+  async loadCourses() {
+    try {
+      const response = await this.courseService.getCourses();
+      this.courses.set(response);
+      // console.table(this.courses());
+    } catch (err) {
+      // console.error(err);
+      this.error.set("Kunde inte ladda någon data");
+    }
   }
 }
--- src/app/services/course-service.ts
+++ src/app/services/course-service.ts
@@ -1,17 +1,19 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 import { Course } from '../interfaces/course'; // Import av interface
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService {
-  url: string = 'https://webbutveckling.miun.se/files/ramschema.json';
+  private url: string = 'https://webbutveckling.miun.se/files/ramschema.json'
 
-  constructor(private http: HttpClient) { }
-
-  getCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>(this.url);
+  http = inject(HttpClient);
+  
+  // Hämta kurser
+  async getCourses(): Promise<Course[]> {
+    const courses = this.http.get<Course[]>(this.url);
+    return await firstValueFrom(courses);
   }
 }
