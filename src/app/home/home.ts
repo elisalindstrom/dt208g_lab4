1659936c429b0e4877c0a1948487ab6a4a3f3eb4
elisalindstrom <elisalindstroem@gmail.com>
2026-05-07 12:18:11 +0200

    flyttat logik till ts + lagt till readme

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -13,13 +13,13 @@ import { CommonModule } from '@angular/common';
 export class Home {
   courses = signal<Course[]>([]);
   error = signal<string | null>(null);
-  filterCourses = signal("");
-  sortCourses = signal("");
+  filterCourses = signal(""); // Signal för filtrering
+  sortCourses = signal(""); // Signal för sortering
 
-  // Inject av service: course-service
+  // Inject av service: använd course-service
   courseService = inject(CourseService);
 
-  // Filtrering
+  // Filtrering utifrån kurskod eller kursnamn
   filteredCourses = computed(() => {
     const filter = this.filterCourses().trim().toLowerCase();
 
@@ -35,23 +35,37 @@ export class Home {
   sortedCourses = computed(() => {
     const sort = this.sortCourses();
 
+    // Kurskod
     if (sort === 'code') {
       return [...this.filteredCourses()].sort((a, b) =>
         a.code.localeCompare(b.code))
     }
 
+    // Kursnamn
     if (sort === 'coursename') {
       return [...this.filteredCourses()].sort((a, b) =>
         a.coursename.localeCompare(b.coursename))
     }
 
+    // Progression
     if (sort === 'progression') {
       return [...this.filteredCourses()].sort((a, b) =>
         a.progression.localeCompare(b.progression))
     }
 
+    // Returnerar alltid ett värde
     return this.filteredCourses();
   })
+
+  // Ändrar signalvärdet för filtrering
+  onCoursesFiltered(filter: string) {
+    this.filterCourses.set(filter);
+  }
+
+  // Ändrar signalvärdet för sortering
+  onCoursesSorted(sort: string) {
+    this.sortCourses.set(sort);
+  }
 
   // Körs vid start
   ngOnInit(): void {
@@ -61,7 +75,6 @@ export class Home {
   // Ser till att service anropas och data hämtas
   async loadCourses() {
     try {
-      // 
       const response = await this.courseService.getCourses();
       this.courses.set(response);
     } catch (err) {
